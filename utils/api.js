2b3e2e3122a6b5e50168f328d5acfe0916d67584
Samuel Path <samuel.path@gmail.com>
2016-10-02 11:15:19 +0200

    big merge

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+export function getEvents(clubId) {
+   let url = 'http://192.168.101.66:8000/scrape/club/' + clubId 
+    + '/week/2016-10-10/championship/EXCELLENCE%20SENIORS%20-%20POULE%20A';
+
+    console.log('url', url);
+    return fetch(url, {
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+}
+
